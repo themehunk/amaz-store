122bd1015ca272f5cfd191e6f1da0f25c59c93f1
iheeresh <heereshd@gmail.com>
2021-12-18 18:30:18 +0530

    amaz store pro issues resolved

--- customizer/js/customizer-control-toggle/customize-cat-slider-toggle.js
+++ customizer/js/customizer-control-toggle/customize-cat-slider-toggle.js
@@ -19,5 +19,21 @@ OPNControlTrigger.addHook( 'amaz-store-toggle-control', function( argument, api 
 			
 			 
 		];	
+		OPNCustomizerToggles ['amaz_store_cat_slide_layout'] = [
+		     {
+				controls:[    
+				'amaz_store_cat_item_no',
+				
+				],
+				callback: function(layout7){
+					if(layout7=='cat-layout-1'){
+					return true;
+					}else{
+					return false;	
+					}
+				}
+			},	
+				
+		];
     });
 })( jQuery );
